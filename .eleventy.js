a0789057a5e4d6be52b4ec62c8724a705e1f15c0
Fridon <fridontetradze@gmail.com>
2022-08-29 17:32:59 +0400

    add reference pirate links

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 const { DateTime } = require("luxon");
+const { libgen, scihub } = require("./lib/pirate-link");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("./src/styles");
@@ -9,6 +10,9 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addFilter("postDate", (dateObj) => {
     return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
+
+  eleventyConfig.addFilter("libgen", (reference) => libgen(reference));
+  eleventyConfig.addFilter("scihub", (reference) => scihub(reference));
 
   eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
 
