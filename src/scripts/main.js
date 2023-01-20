e6bf733f1dc12a64806cdd9e0ee712d6235140d6
Pridon Tetradze <pridontetradze@gmail.com>
2023-01-20 13:54:18 +0400

    add back to top button

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,29 @@
+// Back to top button
+button = document.getElementById("back-to-top");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () {
+  scrollFunction();
+};
+
+button.addEventListener("click", () => {
+  topFunction();
+});
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    button.style.display = "block";
+  } else {
+    button.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
 // creates citation of an article from primordialsoup.info
 // and copies it to clipboard
 function citeArticle(author, date, title, url) {
